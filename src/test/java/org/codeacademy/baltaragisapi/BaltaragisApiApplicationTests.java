09ac70282ae3869af6ab72db84230b26f2fa5edd
Dovydas <ddorofejevas@yahoo.com>
2025-08-20 12:27:18 +0300

    Initial commit

---
+++ src/test/java/org/codeacademy/baltaragisapi/BaltaragisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeacademy.baltaragisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaltaragisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
